b5a841e1c1aca364fc15580d07ba990a7a568ff6
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-16 13:34:44 +0300

    first contribution commit

--- program1..dart
+++ program1..dart
@@ -1,2 +1,8 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+// ## Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+
+
+
+
+
