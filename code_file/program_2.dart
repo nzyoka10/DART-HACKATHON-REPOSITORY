7b05a9539f063e92f8b50796bf10c6d3397cc551
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-16 15:02:20 +0300

    program to perform mathematical operations using functions

---
+++ code_file/program_2.dart
@@ -0,0 +1,71 @@
+
+//~      Program 2: Perform Mathematical Operations with Functions
+//~      Write a Dart program that performs two mathematical operations using functions.
+
+//*  Import the dart math library to access its functionality
+import 'dart:math'; 
+
+void main(){
+  // numbers for operation
+  int num1 = 5;
+  int num2 = 8;
+
+  // Square Root (√)
+  double number = 225;
+  double squareRoot = getSquareRoot(number);
+
+  //* Title
+  print("\n --- Mathematical Operations dart---");
+  
+  print("Sum of ${num1} and ${num2} = ${addition(num1, num2)}");
+  print("Subtraction of ${num1} and ${num2} = ${difference(num1, num2)}");
+  print("Product of ${num1} and ${num2} = ${product(num1, num2)}");
+  print("Division of ${num1} and ${num2} = ${divide(num1, num2)}");
+  print("Modulus of ${num1} and ${num2} = ${modulus(num1, num2)}");
+  print("Square root of $number is: $squareRoot");
+
+
+
+  // print("Square root of ${num3} = ${square_root(square_root)}");
+}
+
+// function to add the two numbers
+int addition(int num1, int num2){
+  return (num1 + num2);
+}
+
+//function to return the difference
+int difference(int num1, int num2){
+  return (num1 - num2);
+}
+
+//function to return the product
+int product(int num1, int num2) {
+  return (num1 * num2);
+}
+
+// function to return the division
+double divide(int num1, int num2) {
+  return (num1 / num2);
+}
+
+// function to return the reminder
+int modulus(int num1, int num2) {
+  return (num1 % num2);
+}
+
+// Function to calculate square root
+double getSquareRoot(double number) {
+  return sqrt(number);
+}
+
+
+
+
+
+
+
+
+
+
+
